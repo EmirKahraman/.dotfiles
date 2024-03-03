6831f7cc1650cdbf590df2a4e53e52c5f1d346f7
Emir Kahraman <70405944+EmirKahraman@users.noreply.github.com>
2024-03-03 06:42:13 -0500

    edit autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -17,12 +17,10 @@ picom --daemon &                # controls transperancy
 #/usr/bin/emacs --daemon &      # if you want to use emacs again uncomment
 sleep 1
 
-### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+### UNCOMMENT ONLY ONE OF THE FOLLOWING TWO OPTIONS! ###
 # 1. Uncomment to restore last saved wallpaper
-#xargs xwallpaper --stretch < ~/.cache/wall &
-# 2. Uncomment to set a random wallpaper on login
-# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
-# 3. Uncomment to set wallpaper with nitrogen
+# xargs xwallpaper --stretch < ~/.cache/wall &
+# 2. Uncomment to set wallpaper with nitrogen
 nitrogen --restore &
 
 # Touchpad Settings
